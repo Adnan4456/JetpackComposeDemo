05fd6db7b4b473638a0fd8f7dd7e827a8b81758c
Adnan4456 <adnan4456@gmail.com>
2022-09-02 23:02:54 +0200

    add switch button and handle state

--- app/src/main/java/com/example/startjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/startjetpackcompose/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.startjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,7 +12,12 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.startjetpackcompose.ui.theme.StartJetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,6 +61,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     //Content will appear here. text and other composeable function.
                     Greeting(name = "Jetpack Compose")
+                    ShowSwitch()
                 }
             }
         }
@@ -68,6 +76,25 @@ fun Greeting(name: String) {
         )
 }
 
+@Composable
+fun ShowSwitch(){
+    val isChecked = remember{
+
+        mutableStateOf(false)
+    }
+
+    Switch(
+        checked = isChecked.value,
+        onCheckedChange = {
+            isChecked.value = it
+        },
+        modifier = Modifier.run {
+            size(20.dp)
+            padding(100.dp)
+
+        }
+    )
+}
 
 @Preview(showBackground = true , name = "light mode")
 //@Preview(showBackground = true , name = "dark mode" ,
